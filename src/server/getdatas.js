5ff290b084c9eb0b4332ae2e235d12c3727e26fa
DellBoy <prova@prova.it>
2022-06-06 16:03:04 +0200

    asd

--- src/server/getdatas.js
+++ src/server/getdatas.js
@@ -1,10 +1,10 @@
-import Axios from "axios";
+/*import Axios from "axios";
 
-function GetdataSkin(props){
+function GetdataSkin(props) {
 
-    Axios.post('http://localhost:3001/getdataskin',{skinid : props.id}).then((response) => {
+    Axios.post('http://localhost:3001/getdataskin', { skinid: props.id }).then((response) => {
         return (response.data[0]);
     });
 };
 
-export default GetdataSkin;
+export default GetdataSkin;*/
