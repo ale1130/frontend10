e140a0194711bfe62bef49b4441ac2718e49999e
ale1130 <ale@gamesolutions.org>
2022-06-07 11:40:10 +0200

    fdf

--- src/server/getdatas.js
+++ src/server/getdatas.js
@@ -3,8 +3,7 @@ import Axios from "axios";
 async function GetdataSkin (id){
 
     Axios.post('http://localhost:3001/getdataskin',{skinid : id}).then(response => {
-        //return response.data[0];
-        //const datiRicevuti = await response.data[0];
+
         return "ciao";
     });
 };
