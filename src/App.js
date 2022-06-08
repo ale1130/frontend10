016a9d9d8d30c86bc2e74244b4f51610c30c1c59
ale1130 <mari.alessandro111@gmail.com>
2022-06-08 17:51:39 +0200

    Impostati valori di root globali per i colori

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, {useState} from "react";
 import {useEffect} from "react";
 import axios from "axios";
-
-import { GlobalStyle } from "./globalStyles";
-
 import {Navbar} from './components/navbar';
 import {GameSection} from './components/navbar';
 import Footer from './components/footer';
@@ -11,21 +8,21 @@ import {PhoneIcon} from './components/icons';
 import {GiftIcon} from './components/icons';
 import {SettingsIcon} from './components/icons';
 import {SelectLanguages} from './components/languagesselector';
-
 import LoginModal from "./components/loginmodal";
 import RegistrationModal from "./components/registration";
-
 import ControlledCarousel from "./components/slider";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Spinner from 'react-bootstrap/Spinner'
+
+import { createGlobalStyle } from 'styled-components';
+import { Megastile } from "./components/superStile";
 
 const skinId = (new URL(window.location.href)).searchParams.get('id');
 const logoDirectory = "https://media.betzonelab.com/skins/logo/";
 
 function App(){
 
-  const [datiSkin, setDatiSkin] = useState([]);
+  const [datiSkin, setDatiSkin] = useState([]); 
 
   const [SKIN, setSKIN] = useState([]);
 
@@ -34,6 +31,8 @@ function App(){
   const [show, setShow] = useState(false);
 
   const [loader, setLoader] = useState(true);
+
+  const [USER, setUSER] = useState([]);
 
   const GetdataSkin = async () =>{
 
@@ -70,6 +69,10 @@ function App(){
 
   },[datiSkin]);
 
+  const Stile = createGlobalStyle`
+  ${Megastile(SKIN)}
+  `;
+
   return (
     <>
 
@@ -82,6 +85,7 @@ function App(){
       </>
       :
       <>
+        <Stile />
         <Navbar
           logo={logoDirectory+SKIN["logo_img"]}
           svggift={<GiftIcon />}
