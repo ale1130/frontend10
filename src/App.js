da42e9c4eada5408357ce7dbaa49107f32b5b2dc
ale1130 <ale@gamesolutions.org>
2022-06-08 12:02:33 +0200

    modifiche

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {PhoneIcon} from './components/icons';
 import {GiftIcon} from './components/icons';
 import {SettingsIcon} from './components/icons';
 import {SelectLanguages} from './components/languagesselector';
-import {Logo} from './components/logo';
+
 import LoginModal from "./components/loginmodal";
 import RegistrationModal from "./components/registration";
 
@@ -21,36 +21,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Spinner from 'react-bootstrap/Spinner'
 
 const skinId = (new URL(window.location.href)).searchParams.get('id');
+const logoDirectory = "https://media.betzonelab.com/skins/logo/";
 
 function App(){
 
-  const logoDirectory = "https://media.betzonelab.com/skins/logo/";
-
   const [datiSkin, setDatiSkin] = useState([]);
+
+  const [SKIN, setSKIN] = useState([]);
 
   const [showReg, setShowReg] = useState(false);
 
   const [show, setShow] = useState(false);
 
-  const [pagina, setPagina]  = useState(
-    <>
-      <Spinner animation="border" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
-    </>
-  );
-
-  const [elements, setElements] = useState([
-    {
-      valore :0, nome:'infoSkin'
-    },
-  ]);
-
-  const setValoreNuovoElemento = (index) => {
-    let newArr = elements;
-    newArr [index].valore = 1;
-    setElements(newArr);
-  }
+  const [loader, setLoader] = useState(true);
 
   const GetdataSkin = async () =>{
 
@@ -61,7 +44,8 @@ function App(){
       .then(response => {
         
         setDatiSkin(response.data[0]);
-        setValoreNuovoElemento(0);
+      }).then(function(){
+        setLoader(false);
       });
     }catch (e){
 
@@ -73,73 +57,63 @@ function App(){
     GetdataSkin();
   },[]);
 
-  var approvato = 0;
-  const nApprovazioni = elements.length;
-
   useEffect(() => {
 
-    var downloadTimer = setInterval(function(){
+    var skinUtilizzo = [];
 
-      elements.map(elemento =>
-        {
-          if(elemento.valore===1){
-            approvato++;
-          }
-        }
-      )
-    
-      if(approvato>=nApprovazioni){
+    const objectArray = Object.entries(datiSkin);
 
-        clearInterval(downloadTimer);
-
-        var oggettoDati = [];
-
-        const objectArray = Object.entries(datiSkin);
-
-        objectArray.forEach(([key, value]) => {
-          oggettoDati[key] = value;
-        });
-
-        setPagina(
-        <>
-          
-          <Navbar
-            logo={logoDirectory+oggettoDati["logo_img"]}
-            svggift={<GiftIcon />}
-            svgsettings={<SettingsIcon />}
-            childLanguage={<SelectLanguages svgphone={<PhoneIcon/>} />}
-            childModalButton = {() => setShow(true)}
-            gamesection={<GameSection />}
-          />
-
-          <LoginModal 
-            modalState={show} 
-            closeModal={() => setShow(false)}
-            openModalReg={() => setShowReg(!showReg)}
-          />
-
-          <ControlledCarousel 
-            openForm={() => setShow(true)}
-          />  
-
-          <RegistrationModal 
-            modalState={showReg} 
-            closeModal={() => setShowReg(false)}
-          />
-
-          <Footer
-            logo={logoDirectory+oggettoDati["logo_img"]}
-            footer_text={oggettoDati["footer_text"]}
-          />
-        </>);
-      }
-
-    }, 1000);
+    objectArray.forEach(([key, value]) => {
+      skinUtilizzo[key] = value;
+      setSKIN(skinUtilizzo);
+    });
 
   },[datiSkin]);
 
   return (
-    pagina
+    <>
+
+      {loader ? 
+
+      <>
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </>
+      :
+      <>
+        <Navbar
+          logo={logoDirectory+SKIN["logo_img"]}
+          svggift={<GiftIcon />}
+          svgsettings={<SettingsIcon />}
+          childLanguage={<SelectLanguages svgphone={<PhoneIcon/>} />}
+          childModalButton = {() => setShow(true)}
+          gamesection={<GameSection />}
+        />
+
+        <LoginModal 
+          modalState={show} 
+          closeModal={() => setShow(false)}
+          openModalReg={() => setShowReg(!showReg)}
+        />
+
+        <ControlledCarousel 
+          openForm={() => setShow(true)}
+        />  
+
+        <RegistrationModal 
+          modalState={showReg} 
+          closeModal={() => setShowReg(false)}
+        />
+
+        <Footer
+          logo={logoDirectory+SKIN["logo_img"]}
+          footer_text={SKIN["footer_text"]}
+        />
+      </>
+      }
+      
+    </>
   );
 
 }
