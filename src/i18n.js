e6f88dadb7a75962f0c84f3a1deeb4addcd27fd6
ale1130 <mari.alessandro111@gmail.com>
2022-06-21 14:29:11 +0200

    ci proviamo

--- src/i18n.js
+++ src/i18n.js
@@ -1,65 +1,3 @@
-<<<<<<< HEAD
-import i18n from "i18next";
-
-import Backend from "i18next-xhr-backend";
-
-import { initReactI18next } from "react-i18next";
-
-
-i18n
-
-  .use(Backend)
-
-  .use(initReactI18next)
-
-  .init({
-
-    lng: "en",   //default language
-
-    fallbackLng: "en", //when specified language translations not present 
-
-//then fallbacklang translations loaded.
-
-    debug: true,
-
-
-       backend: {
-
-      /* translation file path */
-
-      loadPath: "https://cdn.mindbowser.com/assets/i18n/{{ns}}/{{lng}}.json",
-
-    },
- 
-
-/* can have multiple namespace, in case you want to divide a huge translation into smaller pieces and load them on demand */
-
-
-    ns: ["translations"],
-
-    defaultNS: "translations",
-
-    keySeparator: false,
-
-    interpolation: {
-
-      escapeValue: false,
-
-      formatSeparator: ",",
-
-    },
-
-    react: {
-
-      wait: true,
-
-    },
-
-  });
-
-
-export default i18n;
-=======
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
@@ -92,4 +30,3 @@ i18n
   });
 
 export default i18n;
->>>>>>> parent of b700e5a (error)
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import './styles/style.css';
 
 import './i18n';
