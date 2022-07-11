09282301c220c12b63e036031519eaa76d2b9217
ale1130 <mari.alessandro111@gmail.com>
2022-07-11 16:35:26 +0200

    erferf

--- src/components/depositmethod.js
+++ src/components/depositmethod.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Account from "../pages/account";
 
 import { ComponentVoucher } from "../pagesdeposit/voucherdepositcomponent";
@@ -8,8 +8,7 @@ import { ComponentCrypto } from "../pagesdeposit/cryptodepositcomponent";
 import { ComponentBank } from "../pagesdeposit/bankdepositcomponent";
 import { ComponentLidgicash } from "../pagesdeposit/lidgicashdepositcomponent";
 import { ComponentIntouch } from "../pagesdeposit/intouchdepositcomponent";
-import { api, ConvertToAssociativeArray } from "../constants/global";
-import { Loader } from "./spinner";
+import { ConvertToAssociativeArray } from "../constants/global";
 
 import { useTranslation } from "react-i18next";
 
@@ -20,107 +19,58 @@ function DepositMethod (props){
 
     const { t, i18n } = useTranslation();
 
-    const [default_promo, setDefault_promo] = useState(["empty"]);
-    const [loader, setLoader] = useState(true);
-
     const method = props.method;
 
     const methodInfo = ConvertToAssociativeArray(props.countMethods.depositMethods);
 
-    const getDefaultPromoActive = async () =>{
-
-        try {
-    
-            const data = await api
-            .get('rest/defaultpromoactive/:'+USER["id"]+'/')
-            .then(response => {
-      
-              if(response.data.status=="ok" || response.data.status=="error"){
-      
-                setDefault_promo(response.data.dati);
-      
-              }else{
-      
-                alert(t('erroregenerico'));
-              }
-            })
-      
-        } catch (e) {
-    
-            alert(t('erroregenerico'));  
-            console.log(e);
-        }
-    }
-
-    useEffect(()=>{
-        getDefaultPromoActive();
-    },[]);
-
-    useEffect(()=>{
-        if(default_promo!="empty"){
-            setLoader(false);
-        }
-    },[default_promo])
-
     var mainComponent;
 
-    useEffect(()=>{
+    switch(method){
 
-        switch(method){
+        case "pix":
+            mainComponent = <ComponentPix methodInfo ={methodInfo.pix} user={USER} skin={SKIN} />
+        break;
 
-            case "pix":
-                mainComponent = <ComponentPix methodInfo ={methodInfo.pix} user={USER} skin={SKIN} defaultPromo={default_promo} />
-            break;
+        case "voucher":
+            mainComponent = <ComponentVoucher methodInfo ={methodInfo.voucher} user={USER} skin={SKIN} />
+        break;
 
-            case "voucher":
-                mainComponent = <ComponentVoucher methodInfo ={methodInfo.voucher} user={USER} skin={SKIN} defaultPromo={default_promo} />
-            break;
+        case "creditcard":
+            mainComponent = <ComponentCreditCard methodInfo ={methodInfo.creditcard} user={USER} skin={SKIN} />
+        break; 
 
-            case "creditcard":
-                mainComponent = <ComponentCreditCard methodInfo ={methodInfo.creditcard} user={USER} skin={SKIN} defaultPromo={default_promo} />
-            break; 
+        case "pixs2p":
+            mainComponent = <ComponentPix methodInfo ={methodInfo.pixs2p} user={USER} skin={SKIN} />
+        break;
 
-            case "pixs2p":
-                mainComponent = <ComponentPix methodInfo ={methodInfo.pixs2p} user={USER} skin={SKIN} defaultPromo={default_promo} />
-            break;
+        case "cryptoio":
+            mainComponent = <ComponentCrypto methodInfo ={methodInfo.cryptoio} user={USER} skin={SKIN} />
+        break;
 
-            case "cryptoio":
-                mainComponent = <ComponentCrypto methodInfo ={methodInfo.cryptoio} user={USER} skin={SKIN} defaultPromo={default_promo} />
-            break;
+        case "pixm":
+            mainComponent = <ComponentPix methodInfo ={methodInfo.pixm} user={USER} skin={SKIN} />
+        break;
 
-            case "pixm":
-                mainComponent = <ComponentPix methodInfo ={methodInfo.pixm} user={USER} skin={SKIN} defaultPromo={default_promo} />
-            break;
+        case "intouch":
+            mainComponent = <ComponentIntouch methodInfo ={methodInfo.intouch} user={USER} skin={SKIN} />
+        break;
 
-            case "intouch":
-                mainComponent = <ComponentIntouch methodInfo ={methodInfo.intouch} user={USER} skin={SKIN} defaultPromo={default_promo} />
-            break;
+        case "lidgi":
+            mainComponent = <ComponentLidgicash methodInfo ={methodInfo.lidgi} user={USER} skin={SKIN} />
+        break;
 
-            case "lidgi":
-                mainComponent = <ComponentLidgicash methodInfo ={methodInfo.lidgi} user={USER} skin={SKIN} defaultPromo={default_promo} />
-            break;
-
-            case "bank":
-                mainComponent = <ComponentBank methodInfo ={methodInfo.bank} user={USER} skin={SKIN} defaultPromo={default_promo} />
-            break;
-        }
-
-    },[default_promo])
+        case "bank":
+            mainComponent = <ComponentBank methodInfo ={methodInfo.bank} user={USER} skin={SKIN} />
+        break;
+        default:
+            mainComponent = <div>Il metodo di deposito immesso non risulta presente tra quelli disponibili, ti preghiamo di tornare alla pagina precedente</div>
+        break;
+    }
         
     return (
         <>
-            {!loader ? 
-
-            <>
-                <Account countMethods={props.countMethods} paginaAttuale={"deposit"} datiUtente={USER} />
-                {mainComponent}
-            </> 
-            
-            : 
-            
-            <Loader />
-            
-            }
+            <Account countMethods={props.countMethods} paginaAttuale={"deposit"} datiUtente={USER} />
+            {mainComponent} 
         </>
     )
 }
--- src/pagesdeposit/componentpromocode.js
+++ src/pagesdeposit/componentpromocode.js
@@ -1,7 +1,8 @@
 import React, { PureComponent } from "react"
 
 function PromoCode (props){
-console.log(props.inputs)
+
+    const inputs = props.inputs;
 
     return (
         <>
@@ -9,12 +10,12 @@ console.log(props.inputs)
 
                 <hr className="border-hr" />
 
-                <input type="checkbox" name="have_promocode" id="have_promocode" onChange={() => props.handleChangePromocode()} checked={props.inputs.promocode != "nopromo" ? "checked" : ""} /> <label htmlFor="have_promocode">Hai un codice promozionale?</label>
+                <input type="checkbox" name="have_promocode" id="have_promocode" onChange={() => props.handleChangePromocode()} checked={inputs.have_promocode == 1 ? "checked" : ""} /> <label htmlFor="have_promocode">Hai un codice promozionale?</label>
 
-                <div className="row" hidden={props.inputs.have_promocode==1 ? "" : "hidden"}>
+                <div className="row" hidden={inputs.have_promocode==1 ? "" : "hidden"}>
                     <div className="col-sm-12" id="promocode-box">
 
-                        <input type="text" name="promocode" onChange={event => props.handleChange(event)} value={props.inputs.promocode || ""} className="form-control" id="promocode" placeholder="Inserisci qui il codice promozionale" />
+                        <input type="text" name="promocode" onChange={event => props.handleChange(event)} value={inputs.promocode || ""} className="form-control" id="promocode" placeholder="Inserisci qui il codice promozionale" />
                     </div>
                 </div>
 
