43a30f8cb4492887827794801e21c9f1aa23e05d
ale1130 <mari.alessandro111@gmail.com>
2022-07-13 13:35:39 +0200

    vsdgsv

--- src/components/SlickSlider.js
+++ src/components/SlickSlider.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import Slider from "react-slick";
 import { ConvertObjectToArraySlideshow, MEDIA_SLIDESHOWS_WEB_PATH } from '../constants/global';
 
 import { useTranslation } from 'react-i18next';
+
+<script>
+
+    <link
+        rel="stylesheet"
+        type="text/css"
+        charset="UTF-8"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+    />
+    <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+    />
+</script>
 
 export const SlickSlider = (props) => {
 
@@ -14,25 +30,40 @@ export const SlickSlider = (props) => {
 
     const fadeImages = ConvertObjectToArraySlideshow(props.images[0]);
 
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        centerMode: true,
+        adaptiveWidth: true,
+        className: "provola",
+        arrows: false
+    };
+
     return (
 
         <>
-            <div className="margin-slider">
-                <div id="slickSlider" className="slick-slider">
+            <Slider {...settings}>
+                <div className="margin-slider">
+                    <div id="slickSlider" className="slick-slider">
 
-                    {fadeImages.map((fadeImage, index) => (
+                        {fadeImages.map((fadeImage, index) => (
 
-                        <>
-                            <div className="slick-slide">
-                                <div className="slider-item">
-                                    <img src={MEDIA_SLIDESHOWS_WEB_PATH(SKIN) + fadeImage.url} />
-                                    {!logged && <><a href="#" onClick={props.login} className="botton-adv-1">{t('accedi')}</a></>}
-                                </div>
-                            </div>   
-                        </>
-                    ))}
+                            <>
+                                <div className="slick-slide">
+                                    <div className="slider-item">
+                                        <img src={MEDIA_SLIDESHOWS_WEB_PATH(SKIN) + fadeImage.url} />
+                                        {!logged && <><a href="#" onClick={props.login} className="botton-adv-1">{t('accedi')}</a></>}
+                                    </div>
+                                </div>   
+                            </>
+                        ))}
+                    </div>
                 </div>
-            </div>
+            </Slider>
         </>
     )
 }
